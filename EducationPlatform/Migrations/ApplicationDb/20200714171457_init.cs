807582d01010ec3287ea1af6c299b4a6a9553a74
war-man <40898660+war-man@users.noreply.github.com>
2020-07-30 10:20:58 +0700

    Add project files.

---
+++ EducationPlatform/Migrations/ApplicationDb/20200714171457_init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EducationPlatform.Migrations.ApplicationDb
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FactorCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FactorCodeEnabled",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FactorCode",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "FactorCodeEnabled",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
